06741c961f926e688dddf78af246d9318adf8e44
Mario van Straten <mariovanstraten@mac.com>
2020-04-10 15:11:05 +0200

    Further code cleaning

--- script.js
+++ script.js
@@ -18,6 +18,15 @@ let inputs;
 let gameOver;
 let tries = 0;
 
+const loseMessage = document.querySelector(".lose")
+const winMessage = document.querySelector(".win")
+const livesRemaningMessage = document.querySelector(".lives span")
+const displayWrongLetters = document.querySelector(".guessed_letters")
+const diplayCorrectLetters = document.querySelector(".the_word")
+const inputfield = document.querySelector("input")
+const displayCorrectWord = document.querySelector(".lose p span")
+
+
 ////////////////////////////////////////////////
 const wordList = [
   "vis",
@@ -35,10 +44,6 @@ const wordpicker = function (array) {
   return array[index];
 };
 
-
-
-
-
 //  checkt of letter aanwezig is in woord, als er geen letter is meer over zijn geeft hij true
 const wordGuessed = function (word, inputs) {
   let remaining = word.filter(function (letter) {
@@ -49,31 +54,28 @@ const wordGuessed = function (word, inputs) {
 
 // het zichtbaar maken van de win message - status van de game naar true
 const winTheGame = function () {
-  document.querySelector(".win").style.display = "block";
-  gameOver = true;
+  winMessage.style.display = "block";
+  return gameOver = true;
 };
 
 // het zichtbaar maken van de lose message - status van de game naar true
 const lose4 = function () {
-  document.querySelector(".lose").style.display = "block";
+  loseMessage.style.display = "block";
   gameOver = true;
 };
 
 // weergeven van de nog aantal pogingen te gaan.
 const updateTriesDisplay = function (tries) {
+  livesRemaningMessage.innerHTML = 5 - tries;
   return 5 - tries;
-  document.querySelector(".lives span").innerHTML = 5 - tries;
 };
-
-
-
 
 // het weergeven van de foute geraden letters 
 const letters = function (word, inputs) {
   let wrongLetters = inputs.filter(function (letter) {
     return !word.includes(letter);
   });
-  document.querySelector(".guessed_letters").innerHTML = wrongLetters.join(" ");
+  displayWrongLetters.innerHTML = wrongLetters.join(" ");
 };
 
 // kijkt of  de user-input in het woord zit,  als ja: letter, else een streepje
@@ -84,15 +86,10 @@ const theWord = function (word, inputLetterWords) {
     } else {
       return "_";
     }
-
   });
+  diplayCorrectLetters.innerHTML = display.join(" ");
   return display.join(" ");
-  // document.querySelector(".the_word").innerHTML = display.join(" ");
-
 };
-
-
-
 
 const guessLetter = function () {
   // als gameover dan stopt het spel
@@ -102,7 +99,7 @@ const guessLetter = function () {
 
   // neemt de input-veld waarde  en reset de input-veld
   const input1 = document.querySelector("input").value;
-  document.querySelector("input").value = "";
+  inputfield.value = "";
 
   // als de input al in de array aanwezig of de input is leeg dat terug
   if (inputs.includes(input1) || input1 === "") {
@@ -112,7 +109,7 @@ const guessLetter = function () {
   // als de letter niet in het woord zit,  updates de poging met 1, en laat dat zien in de html
   if (!word.includes(input1)) {
     tries++;
-    document.querySelector(".lives span").innerHTML = 5 - tries;
+    livesRemaningMessage.innerHTML = 5 - tries;
   }
 
   // elke input wordt opgeslagen
@@ -142,12 +139,11 @@ function beginTheGameWithPlayer(player1) {
 
   // selecteerd een woord uit een array wordList en laat het zien
   word = wordpicker(wordList).split("");
-  document.querySelector(".lose p span").innerHTML = `"${word.join("")}"`;
-  word;
+  displayCorrectWord.innerHTML = `"${word.join("")}"`;
 
   // start waarde van de pogingen en display dat
   tries = 0;
-  document.querySelector(".lives span").innerHTML = 5 - 0;
+  livesRemaningMessage.innerHTML = 5 - 0;
 
   // lege array voor straks, voor de user input 
   inputs = [];
@@ -157,6 +153,7 @@ function beginTheGameWithPlayer(player1) {
   letters(word, inputs);
 }
 
+
 document.addEventListener("DOMContentLoaded", function () {
   const guessButton = document.querySelector(".guess")
   guessButton.addEventListener("click", guessLetter);
@@ -164,7 +161,7 @@ document.addEventListener("DOMContentLoaded", function () {
   beginTheGameWithPlayer();
 });
 
-module.exports = { wordpicker, theWord, updateTriesDisplay };
+//module.exports = { wordpicker, theWord, updateTriesDisplay };
 
 
 
